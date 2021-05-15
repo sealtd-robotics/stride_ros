4cf87b2418d4003e900987aab665a0dfa99cddf4
Daniel Cheang <dcheang@sealimited.com>
2021-05-14 22:17:22 -0400

    somewhat completed overseer transitions and tested them

--- overseer/scripts/overseer.py
+++ overseer/scripts/overseer.py
@@ -29,10 +29,8 @@ class Overseer(Machine):
         Machine.__init__(self, states=states, initial='initial', auto_transitions=False)
         self.add_transition('initialize', 'initial', 'manual')
         self.add_transition('to_e_stopped', '*', 'e_stopped')
-        self.add_transition('to_manual', ['e_stopped', 'fault', 'gui_stopped'] 'manual')
-        self.add_transition('to_fault', ['manual', 'auto'], 'fault')
-        # self.add_transition('recover', 'fault', 'manual')
-        # self.add_transition('gui_start', 'gui_stopped', 'manual')
+        self.add_transition('to_manual', ['e_stopped', 'fault', 'gui_stopped'], 'manual')
+        self.add_transition('to_fault', '*', 'fault')
         self.add_transition('to_gui_stopped', ['manual', 'auto'], 'gui_stopped')
 
         # GUI
@@ -64,9 +62,9 @@ class MotorController:
         self.topic_name = topic_name
         self.error_word = 0
 
-        rospy.Subscriber('/motor_controller/{}/state'.format(self.topic_name), Int32, self.set_error_word, queue_size=10)
+        rospy.Subscriber('/motor_controller/{}/error_word'.format(self.topic_name), Int32, self.set_error_word, queue_size=10)
 
-    def set_error_word(msg):
+    def set_error_word(self, msg):
         self.error_word = msg.data
 
 def are_mcs_bad(mcs): # mcs = motor controllers (plural)
@@ -79,26 +77,26 @@ def are_mcs_bad(mcs): # mcs = motor controllers (plural)
 
 class Gui:
     def __init__(self):
-        self.is_motion_stop_clicked = False
-        self.is_motion_start_clicked = False
-        self.is_fault_recovery_clicked = False
-        rospy.Subscriber('/is_motion_stop_clicked', Empty, self.motion_stop_callback, queue_size=20)
-        rospy.Subscriber('/is_motion_start_clicked', Empty, self.motion_start_callback, queue_size=20)
-        rospy.Subscriber('/is_fault_recovery_clicked', Empty, self.fault_recovery_callback, queue_size=20)
+        self.is_stop_clicked = False
+        self.is_start_clicked = False
+        self.is_recover_clicked = False
+        rospy.Subscriber('/gui/stop_clicked', Empty, self.stop_callback, queue_size=20)
+        rospy.Subscriber('/gui/start_clicked', Empty, self.start_callback, queue_size=20)
+        rospy.Subscriber('/gui/recover_clicked', Empty, self.recover_callback, queue_size=20)
 
-    def motion_stop_callback(self):
-        self.is_motion_stop_clicked = True
+    def stop_callback(self, msg):
+        self.is_stop_clicked = True
 
-    def motion_start_callback(self):
-        self.is_motion_start_clicked = True
+    def start_callback(self, msg):
+        self.is_start_clicked = True
 
-    def is_fault_recovery_callback(self):
-        self.is_fault_recovery_clicked = True
+    def recover_callback(self, msg):
+        self.is_recover_clicked = True
 
 class Handheld:
     def __init__(self):
         self.is_estop_pressed = False
-        rospy.Subscriber('/is_estop_pressed', Bool, self.estop_callback, queue_size=20)
+        rospy.Subscriber('/handheld/is_estop_pressed', Bool, self.estop_callback, queue_size=20)
 
     def estop_callback(self, msg):
         self.is_estop_pressed = msg.data
@@ -126,40 +124,42 @@ if __name__ ==  '__main__':
     while not rospy.is_shutdown():
         if overseer.state == 'manual':
             if handheld.is_estop_pressed:
-                overseer.to_e_stopped()
+                overseer.to_e_stopped()#
             elif are_mcs_bad(mcs):
-                overseer.to_fault()
-            elif gui.is_motion_stop_clicked:
-                gui.is_motion_stop_clicked = False
-                overseer.to_gui_stopped()
+                overseer.to_fault()#
+            elif gui.is_stop_clicked:
+                gui.is_stop_clicked = False
+                overseer.to_gui_stopped()#
         elif overseer.state == 'auto':
             if handheld.is_estop_pressed:
                 overseer.to_e_stopped()
             elif are_mcs_bad(mcs) or 0: # add gps_bad condition here
                 overseer.to_fault()
-            elif gui.is_motion_stop_clicked:
-                gui.is_motion_stop_clicked = False
+            elif gui.is_stop_clicked:
+                gui.is_stop_clicked = False
                 overseer.to_gui_stopped()
         elif overseer.state == 'e_stopped':
             if not handheld.is_estop_pressed:
                 if are_mcs_bad(mcs):
-                    overseer.to_fault()
+                    overseer.to_fault()#
                 else:
-                    overseer.to_manual()
+                    overseer.to_manual()#
         elif overseer.state == 'fault':
             if handheld.is_estop_pressed:
-                overseer.to_e_stopped()
-            elif gui.is_fault_recovery_clicked
-                gui.is_fault_recovery_clicked = False
+                overseer.to_e_stopped()#
+            elif gui.is_recover_clicked:
+                gui.is_recover_clicked = False
                 if not are_mcs_bad(mcs):
-                    overseer.to_manual()
+                    overseer.to_manual()#
         elif overseer.state == 'gui_stopped':
-            if gui.is_motion_start_clicked:
-                gui.is_motion_start_clicked = False
-                if not are_mcs_bad(mcs):
-                    overseer.to_manual()
+            if gui.is_start_clicked:
+                gui.is_start_clicked = False
+                if are_mcs_bad(mcs):
+                    overseer.to_fault()#
+                else:
+                    overseer.to_manual()#
             elif handheld.is_estop_pressed:
-                overseer.to_e_stopped()
+                overseer.to_e_stopped()#
             
 
 
