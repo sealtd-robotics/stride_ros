75eefc7db7573ded61951effac6575168c71f0ab
Daniel Cheang <dcheang@sealimited.com>
2022-07-30 18:45:07 -0400

    Send out executing command message to GUI

--- stride_ws/src/networking/scripts/websocket_server.py
+++ stride_ws/src/networking/scripts/websocket_server.py
@@ -134,7 +134,7 @@ class RosInterface:
         rospy.Subscriber('/battery_voltage', Float32, self.subscriber_callback_7, queue_size=1)
         rospy.Subscriber('/target', TargetVehicle, self.subscriber_callback_8, queue_size=1)
         rospy.Subscriber('/csv_converted', Empty, self.subscriber_callback_9, queue_size=1)
-
+        rospy.Subscriber('/robot_commander/command_message', String, self.subscriber_callback_10, queue_size=20)
 
         # GPS Subcribers
         rospy.Subscriber('/an_device/NavSatFix', NavSatFix, self.gps_subscriber_callback_1, queue_size=1) # time.sleep() in callback for throttling, used with queue_size=1
@@ -248,6 +248,9 @@ class RosInterface:
         csvCreatedMsg = json.dumps({'type': 'createdCSV',}, ensure_ascii = False).encode('utf8')
         reactor.callFromThread(self.websocket.sendMessage, csvCreatedMsg, False)
 
+    def subscriber_callback_10(self, msg):
+        command_message = json.dumps({'type': 'commandMessage', 'commandMessage': msg.data}, ensure_ascii = False).encode('utf8')
+        reactor.callFromThread(self.websocket.sendMessage, command_message, False)
 
     # Motor Controller Callbacks
     # Left Front
