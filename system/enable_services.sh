b222691ae1340c1547994aad638b940d2fa47593
ponaravind <mponaravind@gmail.com>
2023-07-13 14:37:54 -0400

    Revert "roscore service change to source bashrc before starting"
    This reverts commit c55a32f0782db231564a03160385962835700c7c.

--- system/enable_services.sh
+++ system/enable_services.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 CURRENT_USER=$SUDO_USER
+ROS_DISTRO=melodic
 
 dir_init=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $dir_init
 
 # Copy .service file to /etc/systemd/system
-cp roscore.service /etc/systemd/system/$CURRENT_USER@roscore.service
+cp roscore.service /etc/systemd/system/$ROS_DISTRO@roscore.service
 cp can_bus.service /etc/systemd/system/$CURRENT_USER@can_bus.service
 cp stride_top_level.service /etc/systemd/system/$CURRENT_USER@stride_top_level.service
 cp gpio.service /etc/systemd/system/$CURRENT_USER@gpio.service
 
 # Enable service to start at boot
-systemctl enable $CURRENT_USER@roscore.service
+systemctl enable $ROS_DISTRO@roscore.service
 systemctl enable $CURRENT_USER@can_bus.service
 systemctl enable $CURRENT_USER@stride_top_level.service
 systemctl enable $CURRENT_USER@gpio.service
