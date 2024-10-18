28e8ae13f0a0113047142d56f330e56cd447e1a9
Nick Helber <nhelber@sealimited.com>
2024-10-18 16:17:10 -0400

    Set sv example script back to default

--- example_scripts/subject_vehicle_trigger.py
+++ example_scripts/subject_vehicle_trigger.py
@@ -23,14 +23,12 @@ target_heading = 228 # Vehicle heading in degrees. Used in wait_for_vehicle_posi
 target_velocity = 3 # Vehicle speed in m/s. Used in wait_for_vehicle_velocity().
 
 ### Trigger using subject vehicle position and heading
-# rc.move_until_index(2, 0.1, 45) #Move at 2 m/s with 0.1g acceleration to index 45
-# rc.decel_to_stop_at_index(50) #Decelerate and stop at index 50
-# rc.wait_for_vehicle_position(target_lat, target_long, target_heading) #Wait for vehicle to trigger desired position/heading
-# rc.move_until_index(2, 0.1, 95) #Move at 2 m/s with 0.1g acceleration to index 95
-# rc.decel_to_stop_at_index(100) #Decelerate and stop at index 100
+rc.move_until_index(2, 0.1, 45) #Move at 2 m/s with 0.1g acceleration to index 45
+rc.decel_to_stop_at_index(50) #Decelerate and stop at index 50
+rc.wait_for_vehicle_position(target_lat, target_long, target_heading) #Wait for vehicle to trigger desired position/heading
+rc.move_until_index(2, 0.1, 95) #Move at 2 m/s with 0.1g acceleration to index 95
+rc.decel_to_stop_at_index(100) #Decelerate and stop at index 100
 ## End test
-# rc.custom_compensation(1,1,1,1,1,1,1)
-rc.move_until_end_of_path(2, 0.1)
 
 ### Trigger using subject vehicle velocity
 # rc.move_until_index(2, 0.1, 45) #Move at 2 m/s with 0.1g acceleration to index 45
